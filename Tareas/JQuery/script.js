a0857ee8546990dbcf31ca2e65a7e3a4ff209f6b
Victor Parra <110433475+Victorparrd@users.noreply.github.com>
2024-02-20 13:30:36 -0300

    Listado mejorado

--- Tareas/JQuery/script.js
+++ Tareas/JQuery/script.js
@@ -4,12 +4,22 @@ $(document).ready(function () {
             url: 'https://rickandmortyapi.com/api/character',
             method: 'GET',
             success: function (characters) {
-                var characterList = $("#characterList");
-                characterList.empty();
+                var tableBody = $(".table-group-divider");
+                tableBody.empty();
 
                 characters.results.forEach(function (character) {
-                    var li = $("<li>").text(character.name + " - " + character.species);
-                    characterList.append(li);
+                    var row = $("<tr>");
+                    var nameCell = $("<td>").text(character.name);
+                    var photoCell = $("<td>").append($("<img>").attr("src", character.image).attr("alt", character.name));
+                    var statusCell = $("<td>").text(character.status);
+                    var specieCell = $("<td>").text(character.species);
+
+                    row.append(nameCell);
+                    row.append(photoCell);
+                    row.append(statusCell);
+                    row.append(specieCell);
+
+                    tableBody.append(row);
                 });
             },
         });
